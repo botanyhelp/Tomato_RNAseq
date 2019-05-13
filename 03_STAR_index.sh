0e8e69da308bf4560e728ceeb22b2fe717dea2e1
xiaodong yang <xiaodongy86@gmail.com>
2019-05-13 17:23:52 -0400

    Update 03_STAR_index.sh

--- 03_STAR_index.sh
+++ 03_STAR_index.sh
@@ -3,9 +3,9 @@ THREADS=6
 GENOME_INDEX_DIR=/data5/tomato_RNAseq/STAR_INDEX
 FASTA_FILES=/data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/ftp.solgenomics.net/genomes/Solanum_lycopersicum/assembly/build_3.00/S_lycopersicum_chromosomes.3.00.fa
 GTF_FILE=/data/users/xzy50/ITAG3.0_gene_models.gff
-
+ulimit -n 2048
 mkdir -p ${GENOME_INDEX_DIR}
-STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE}
+STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE} --sjdbGTFchrPrefix SL3.0ch
 
 
 
