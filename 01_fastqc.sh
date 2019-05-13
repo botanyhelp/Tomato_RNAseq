bda8d850fd63e6c207f4ef1657aa685860fa966b
xiaodong yang <xiaodongy86@gmail.com>
2019-05-13 13:00:02 -0400

    Create 01_fastqc.sh

---
+++ 01_fastqc.sh
@@ -0,0 +1,20 @@
+```
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RDrP1_CAGATC_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RDrP1_CAGATC_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RDrP2_ACTTGA_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RDrP2_ACTTGA_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RDrP3_GATCAG_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RDrP3_GATCAG_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RRCtlP1_TGACCA_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RRCtlP1_TGACCA_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RRCtlP2_ACAGTG_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RRCtlP2_ACAGTG_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RRCtlP3_GCCAAT_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RRCtlP3_GCCAAT_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RutgersWtP1_ATCACG_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RutgersWtP1_ATCACG_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RutgersWtP2_CGATGT_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RutgersWtP2_CGATGT_L002_R2_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RutgersWtP3_TTAGGC_L002_R1_001.fastq.gz
+ts fastqc -t 4 fastq --outdir=/data5/tomato_RNAseq/FastQC/ /data5/tomato_RNAseq/RutgersWtP3_TTAGGC_L002_R2_001.fastq.gz
+```
