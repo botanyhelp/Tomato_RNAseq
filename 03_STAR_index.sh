c5a18f12d68e7c669cfeabf6cd190d506198a5dc
xiaodong yang <xiaodongy86@gmail.com>
2019-05-13 15:03:07 -0400

    Update 03_STAR_index.sh

--- 03_STAR_index.sh
+++ 03_STAR_index.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 THREADS=6
 GENOME_INDEX_DIR=/data5/tomato_RNAseq/STAR_INDEX
-FASTA_FILES=/data/genomes/TAIR10/TAIR10_chr_all.fa
-GTF_FILE=/data/genomes/TAIR10/Arabidopsis_thaliana.TAIR10.37.gtf
+FASTA_FILES=/data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/ftp.solgenomics.net/genomes/Solanum_lycopersicum/assembly/build_3.00/S_lycopersicum_chromosomes.3.00.fa
+GTF_FILE=
 
 mkdir -p ${GENOME_INDEX_DIR}
 STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE}
