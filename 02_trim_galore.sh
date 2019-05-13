a20543bc40633a67f68bae5ba99be04bfb6513df
xiaodong yang <xiaodongy86@gmail.com>
2019-05-13 14:50:18 -0400

    Create 02_trim_galore.sh

---
+++ 02_trim_galore.sh
@@ -0,0 +1,13 @@
+```
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RDrP1 /data5/tomato_RNAseq/RDrP1_CAGATC_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RDrP1_CAGATC_L002_R2_001.fastq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RDrP2 /data5/tomato_RNAseq/RDrP2_ACTTGA_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RDrP2_ACTTGA_L002_R2_001.fastq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RDrP3 /data5/tomato_RNAseq/RDrP3_GATCAG_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RDrP3_GATCAG_L002_R2_001.fastq.gz
+
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RRCtlP1 /data5/tomato_RNAseq/RRCtlP1_TGACCA_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RRCtlP1_TGACCA_L002_R2_001.fastq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RRCtlP2 /data5/tomato_RNAseq/RRCtlP2_ACAGTG_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RRCtlP2_ACAGTG_L002_R2_001.fastq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RRCtlP3 /data5/tomato_RNAseq/RRCtlP3_GCCAAT_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RRCtlP3_GCCAAT_L002_R2_001.fastq.gz
+
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RutgersWtP1 /data5/tomato_RNAseq/RutgersWtP1_ATCACG_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RutgersWtP1_ATCACG_L002_R2_001.fastq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RutgersWtP2 /data5/tomato_RNAseq/RutgersWtP2_CGATGT_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RutgersWtP2_CGATGT_L002_R2_001.fastq.gz
+ts /usr/local/bin05169/trim_galore --phred33 --paired --gzip --fastqc --length 30 -o /data5/tomato_RNAseq/trim_galore/RutgersWtP3 /data5/tomato_RNAseq/RutgersWtP3_TTAGGC_L002_R1_001.fastq.gz /data5/tomato_RNAseq/RutgersWtP3_TTAGGC_L002_R2_001.fastq.gz
+```
