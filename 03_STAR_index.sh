cea28dc78b210457175d9eb1c499689174b3916e
xiaodong yang <xiaodongy86@gmail.com>
2019-05-14 09:27:26 -0400

    Update 03_STAR_index.sh

--- 03_STAR_index.sh
+++ 03_STAR_index.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#download genome and gtf from ensemblgenomes
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/gtf/solanum_lycopersicum/Solanum_lycopersicum.SL3.0.43.gtf.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.1.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.2.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.3.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.4.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.5.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.6.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.7.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.8.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.9.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.10.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.11.fa.gz
+# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/fasta/solanum_lycopersicum/dna/Solanum_lycopersicum.SL3.0.dna.chromosome.12.fa.gz
+
 THREADS=6
 GENOME_INDEX_DIR=/data5/tomato_RNAseq/STAR_INDEX
 FASTA_FILES=/data5/tomato_RNAseq/genome/Solanum_lycopersicum.SL3.0/Solanum_lycopersicum.SL3.0.dna.fa
@@ -6,15 +21,4 @@ GTF_FILE=/data5/tomato_RNAseq/genome/Solanum_lycopersicum.SL3.0.43.gtf
 ulimit -n 2048
 #mkdir -p ${GENOME_INDEX_DIR}
 STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE} 
-
-
-# wget ftp://ftp.ensemblgenomes.org/pub/plants/release-43/gtf/solanum_lycopersicum/Solanum_lycopersicum.SL3.0.43.gtf.gz
-#THREADS=6
-#GENOME_INDEX_DIR=/data5/tomato_RNAseq/STAR_INDEX
-#FASTA_FILES=/data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/ftp.solgenomics.net/genomes/Solanum_lycopersicum/assembly/build_3.00/S_lycopersicum_chromosomes.3.00.fa
-#GTF_FILE=/data/users/xzy50/Solanum_lycopersicum.SL3.0.43.gtf
-#ulimit -n 2048
-#mkdir -p ${GENOME_INDEX_DIR}
-#STAR --runMode genomeGenerate --runThreadN ${THREADS} --genomeDir ${GENOME_INDEX_DIR} --genomeFastaFiles ${FASTA_FILES} --sjdbGTFfile ${GTF_FILE} --sjdbGTFchrPrefix SL3.0ch
-
 
